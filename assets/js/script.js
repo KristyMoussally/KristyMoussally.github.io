8d7b1057ccf6b72e41ab54270a40cc9568d35398
KristyMoussally <2242744@cmontmorency.qc.ca>
2023-11-10 18:05:21 -0500

    Ps4.1 Réparer block css + commencer ps4.2

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,9 +6,7 @@ const chapters = {
     titre: "Avant de commencer !",
     description:
       "Vous vous réveillez dans un monde surréel. La sensation d'être dans ce rêve lucide vous rend mal à l'aise. Votre but est de sortir de ce cauchemar.",
-    video: "/assets/video/accueil.mp4", // Premier vidéo
-    image: "/assets/images/accueil.jpg",
-
+    video: "/assets/video/accueil.mp4",
     buttons: [
       {
         titre: "➤ Commencer l'histoire intéractif",
@@ -21,7 +19,6 @@ const chapters = {
     description:
       "Vous marchez dans un chemin étroit qui vous mène vers un intersection. Un des chemin vous mène vers une forêt remplie des yeux puis l'autre une plaine déserte.",
     image: "/assets/images/plaine.jpg",
-
     buttons: [
       {
         titre: "➤ Aller à la plaine",
@@ -38,7 +35,6 @@ const chapters = {
     description:
       "Plus ce que vous marchez dans la forêt, vous sentez que votre tête tourne en rond. Des hallucinations vous hantents jusqu'à quand vos jambes perd l'équilibre.",
     image: "/assets/images/foret.jpg",
-
     buttons: [
       {
         titre: "➤ Retour vers le début",
@@ -51,7 +47,6 @@ const chapters = {
     description:
       "Vous continuez votre chemin dans la plaine. Vous sentez que quelque chose vous suit...Vous n'êtes pas touts seuls. Vous remarquez qu'il il y a une sorte de porte obscure devant vous. ",
     image: "/assets/images/porte_sombre.jpg",
-
     buttons: [
       {
         titre: "➤ Se tourner et attaquer la créature",
@@ -67,9 +62,7 @@ const chapters = {
     titre: "Quelle belle imagination vous avez !",
     description:
       "Vous vous tournez puis vous préparez à s'élancer. Mais avant de faire cela, vous voyez un trou noir qui avance vers vous. Stupéfait, quand vous reouvrit les yeux pour comprendre ce que vous avez vu, vous vous retrouvez dans l'obscurité total.",
-    video: "/assets/video/creature.mp4", // Deuxième vidéo --> À mettre
-    image: "/assets/images/creature.jpg",
-
+    video: "/assets/video/creature.mp4",
     buttons: [
       {
         titre: "➤ Retour vers l'accueil",
@@ -98,7 +91,6 @@ const chapters = {
     description:
       "En buvant le thé, vous sentez un peu étourdi. Ceci cause une voix qui donne transmet un code. Celui-ci devra être important, prenez note.",
     image: "/assets/images/the.jpg",
-
     buttons: [
       {
         titre: "➤ Aller vers la dernière chambre",
@@ -112,7 +104,6 @@ const chapters = {
     description:
       "La cuisine ressemble est remplie dans un silence très fort. Malheureusement, vous n'avez rien trouvez puis vous décida d'explorer un peu plus. ",
     image: "/assets/images/cuisine_vide.jpg",
-
     buttons: [
       {
         titre: "➤ Aller vers la dernière chambre",
@@ -125,7 +116,6 @@ const chapters = {
     description:
       "Vous explorez le reste de la maison puis vous trouvez se que vous pensez être la sortie. ",
     image: "/assets/images/maison.jpg",
-
     buttons: [
       {
         titre: "➤ Partir tout de suite",
@@ -142,7 +132,6 @@ const chapters = {
     description:
       "Vous décidez de sortie mais ,quand vous ouvrez la porte, vous vous retrouvez vers le tout début de votre `rêve`.",
     image: "/assets/images/coincer.jpg",
-
     buttons: [
       {
         titre: "➤ Retour vers le début",
@@ -155,7 +144,6 @@ const chapters = {
     description:
       "Vous décidez de réexplorer la maison pour bien être sûr que vous n'avez pas oublié quelque chose. Cette action intelligente vous permet de resevoir une montre dont les aiguilles ne marche pas. Par chance, vous savez que cette montre est la clé de la sortie. Vous devez mettre la bonne heure.",
     image: "/assets/images/montre.webp",
-
     buttons: [{ titre: "➤ Mettre le code", destination: "Twist" }], // Ce chapitre n'a pas réellement une destination, car c'est un chapitre unique (twist) [Change manuellement dans la fonction]
   },
   fin: {
@@ -187,9 +175,8 @@ const chapters = {
 // Ps3 (Réparer + Expliquer par le prof)
 
 const contenueJeu = document.querySelector("#jeu");
-const title = document.querySelector("h1");
+const title = document.querySelector("h2");
 const text = document.querySelector("p");
-const contenueImage = document.querySelector(".petitimg");
 const buttons = document.querySelector(".boutons");
 const divVisuel = document.getElementById("visuel");
 
@@ -212,26 +199,27 @@ function goToChapter(key) {
 
   title.innerHTML = chapitre.titre;
   text.innerHTML = chapitre.description;
+  divVisuel.innerHTML = '';
 
   //  PRoblème aux vidéos
 
   // Appliquer si video existant (muet + boucle)
-  if (chapitre.video !== false) {
+  if (chapitre.video != undefined) {
     const videoContenue = document.createElement("video");
     videoContenue.src = chapitre.video;
     videoContenue.classList.add("petitimg");
-    // car je travail sur chrome puis ce navigateur à une function différente sur les vidéos
-    videoContenue.setAttribute("allow", "autoplay");
-
     videoContenue.play();
-
-    videoContenue.setAttribute("loop", "");
-    videoContenue.setAttribute("autoplay", "");
-    console.log(videoContenue);
+    videoContenue.loop = true;
+    videoContenue.autoplay = true;
+    videoContenue.muted = true;
+  
     divVisuel.appendChild(videoContenue);
-    divVisuel.removeChild(contenueImage);
   } else {
-    contenueImage.src = chapitre.image;
+    const imageContenue = document.createElement("img");
+    imageContenue.classList.add("petitimg")
+    //<img src="assets/images/accueil.jpg" alt="photo" class="petitimg" />
+    imageContenue.src = chapitre.image;
+    divVisuel.appendChild(imageContenue);
   }
 
   // créé une nouvelle balise paragraphe "p"
@@ -254,6 +242,7 @@ function goToChapter(key) {
       const msgNumber = document.createElement("input");
       // Donner un name à l'input pour l'appeler après
       msgNumber.setAttribute("name", "msgNumberEcrit");
+      msgNumber.classList.add("inputold")
       buttons.appendChild(msgNumber);
 
       const newBtn = document.createElement("button");
@@ -285,6 +274,30 @@ function goToChapter(key) {
     }
   }
 }
+
+
+
+const btnHelpQuestion = document.getElementById("help");
+const btnDelete = document.getElementById("x");
+const btnFermer = document.getElementById("goBack")
+const popupX = document.getElementById("popUpDelete")
+
+btnHelpQuestion.addEventListener("click", () => {
+  let paraMsg = document.getElementById("msgQuestion");
+  paraMsg.textContent = "Tu as seulement besoin de choisir votre choix."
+
+  popupX.style.display = "block"
+})
+
+btnDelete.addEventListener("click", () => {
+ // alert("Bonne essaie, mais tu restes coincé ici")
+ popupX.style.display = "block"
+ chapitre.setAttribute("blocked")
+})
+
+btnFermer.addEventListener("click", ()=> {
+  popupX.style.display = "none"
+})
 
 //quand on ouvre la page -> montre directement la page d'accueil
 goToChapter("accueil");
